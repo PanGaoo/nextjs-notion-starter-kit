35e79d43099d8ca7243cda57b5565214e4ac100b
PanGaoo <94620400+PanGaoo@users.noreply.github.com>
2022-07-23 17:22:04 +0800

    Update site.config.ts
    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,16 +2,18 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: 'f940238f42524db88fa18c3a6b874bc4',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
+
+
   // basic site info (required)
   name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  domain: 'https://www.notion.so/Quick-Note-f940238f42524db88fa18c3a6b874bc4',
+  author: 'GaoPan',
 
   // open graph metadata (optional)
   description: 'Example Next.js Notion Starter Kit Site',
